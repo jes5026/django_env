03ee6787ebc83b2903848c393f3b489612b0b205
jes5026 <jes5026@gmail.com>
2021-06-08 18:39:58 -0700

    test1

--- anc/anc/settings.py
+++ anc/anc/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*xsjw3=*u0z4ri4nvi$2m15b1vk&nei=^g^-@4th&*s*pv66y@'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
